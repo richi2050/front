0b33f615d09c2b24ce970e68ab3dae2ed50ea8bf
Ricardo Lugo Recillas <ricardo.lugo.recillas@gmail.com>
2019-03-14 23:20:18 -0600

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+  title = 'musify';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(){
+    this.user = new User('','','','','','ROLE_USER','');
+  }
+
+
 }
